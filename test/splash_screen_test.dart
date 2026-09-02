26a9dd447ccce9786f40166ab1569664d9b90915
tvc-ext <tvc.ext@gmail.com>
2026-09-02 08:54:28 +0530

    Update splash branding test

--- test/splash_screen_test.dart
+++ test/splash_screen_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('splash introduces CurioVerse and continues automatically',
+  testWidgets('splash introduces Euphoriks Quizzie and continues automatically',
       (tester) async {
     var finished = false;
     await tester.pumpWidget(
@@ -12,9 +12,9 @@ void main() {
       ),
     );
 
-    expect(find.text('CurioVerse'), findsOneWidget);
-    expect(find.text('Big curiosity. Brilliant adventures.'), findsOneWidget);
-    expect(find.byType(Image), findsOneWidget);
+    expect(find.text('QUIZZIE'), findsOneWidget);
+    expect(find.text('by EUPHORIKS'), findsOneWidget);
+    expect(find.text('Explore • Play • Learn'), findsOneWidget);
 
     await tester.pump(const Duration(milliseconds: 2200));
     expect(finished, isTrue);
